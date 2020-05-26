da9c667335e3a827e778eeb5dfc3ceab657be192
Michael Tsai <huanlin.tsai@gmail.com>
2020-05-27 03:15:21 +0800

    完成批次建立折價券的程式。

--- LeanpubApiClient/Tests/CreateCoupons/Program.cs
+++ LeanpubApiClient/Tests/CreateCoupons/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using LeanpubApi.Client;
 using LeanpubApi.Core.Model;
@@ -8,6 +9,9 @@ namespace CreateCoupons
 {
     class Program
     {
+        static string apiKey;
+        static LeanpubApiClient apiClient;
+
         static async Task Main(string[] args)
         {
             if (args.Length < 1)
@@ -24,10 +28,15 @@ namespace CreateCoupons
                 return;
             }
 
-            var apiKey = (await File.ReadAllTextAsync(".apikey")).Trim();
+            apiKey = (await File.ReadAllTextAsync(".apikey")).Trim();
 
-            var client = new LeanpubApiClient(apiKey);
+            apiClient = new LeanpubApiClient(apiKey);
 
+            await BatchCreateCoupons(bookId, "dinet-miniasp-jun", 11, 40);
+        }
+
+        public static async Task CreateCoupon(string bookId)
+        {
             var coupon = new CouponForCreation
             {
                 coupon_code = "test1234",
@@ -39,9 +48,47 @@ namespace CreateCoupons
                 note = string.Empty
             };
 
-            if (await client.CreateCouponAsync(bookId, coupon))
+            if (await apiClient.CreateCouponAsync(bookId, coupon))
             {
                 Console.WriteLine($"Coupon created: {coupon.coupon_code}");
+            }
+        }
+
+        public static async Task BatchCreateCoupons(string bookId, string couponPrefix, int startNumber, int count)
+        {
+            const string Characters = "23456789abcdefghjkmnpqrstuvwxyz";
+
+            var r = new Random((int)DateTime.Now.Ticks);
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = startNumber + i;
+                var coupon = new CouponForCreation
+                {
+                    coupon_code = $"{couponPrefix}{number:00}-{GetRandomCode()}",
+                    start_date = DateTime.Now.Date.AddDays(-1), // 確保立刻生效
+                    end_date = DateTime.Now.AddYears(1).Date, // 一年內有效
+                    max_uses = 1, // 只能使用一次
+                    discounted_price = 0.0, // 零元結帳
+                    suspended = false, // 立刻生效
+                    note = string.Empty
+                };
+
+                if (await apiClient.CreateCouponAsync(bookId, coupon))
+                {
+                    Console.WriteLine($"Coupon created: {coupon.coupon_code}");
+                }
+            }
+
+            string GetRandomCode()
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < 4; i++)
+                {
+                    int index = r.Next(0, Characters.Length - 1);
+                    sb.Append(Characters[index]);
+                }
+                return sb.ToString();
             }
         }
     }
